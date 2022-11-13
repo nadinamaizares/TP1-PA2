3895d45fcba7723ee3d488adbd592d3e42a92ffb
nadinamaizares <112122717+nadinamaizares@users.noreply.github.com>
2022-11-13 12:23:46 -0300

    Add files via upload

---
+++ Console_App_TP1-master/Console_App_TP1/Calcs.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Console_App_TP1
+{
+    enum Dias { Domingo = 1, Lunes = 2, Martes = 3, Miercoles = 4, Jueves = 5, Viernes = 6, Sabado = 7 }
+    public enum Color { Blanco, Azul, Negro }
+
+    public class Calcs
+    {
+        
+
+        static void Main(string[] args)
+        {
+        }
+
+        // Ejercicio 1
+                
+        public static int Sumar(int a, int b)
+        {
+            return a + b;
+
+        }
+
+
+        // Ejercicio 2
+
+        public static string cadenacortada(string cadena)
+        {
+
+            string resultado = cadena.Substring( 0, cadena.Length - 4 );
+
+            return resultado;
+
+        }
+
+
+        // Ejercicio 3
+        public static string quehoraes()
+        {
+            string hora = DateTime.Now.ToString("dd-MM-yy hh:mm");
+
+            return hora;
+        }
+
+
+        // Ejercicio  4 y 5
+
+
+        // Comentario de una sola línea
+
+        /**
+         * Comentario de varias líneas
+         */
+
+
+
+
+        //  Ejercicio Enumeraciones 1 y 2
+
+        public static string DiasSemana(int i)
+        {
+            Dias dia = (Dias)i;
+            return "Hoy es día " + dia.ToString();
+        }
+
+
+
+
+        //  Ejercicio Conversiones 1
+
+
+        public static bool Conver_V_F(int a)
+        {
+            return Convert.ToBoolean(a);
+        }
+
+
+
+        //  Ejercicio Conversiones 2
+
+        public static bool BoolParseado(string a)
+        {
+            return bool.Parse(a);
+        }
+
+
+
+        //  Ejercicio Conversiones 3
+
+        public static bool Parseado(string a)
+        {
+            return bool.TryParse(a, out bool resultado);
+        }
+
+
+
+        //  Ejercicio Casteo 1
+
+        public static int ConvertirDoubleAInt(double a)
+        {
+            return (int)a;
+        }
+
+
+
+        //  Ejercicio Casteo 2
+
+        public static decimal ConvertirIntADecimal(int a)
+        {
+            return a;
+        }
+
+
+        //  Ejercicio Casteo 3
+
+        public static short ConvertirIntAShort(int a)
+        {
+            return (short)a;
+        }
+
+
+
+        //  Ejercicio Switch 
+
+        public static string QueColorEs(Color color)
+        {
+            switch (color)
+            {
+                case Color.Blanco:
+                    return "Este es Blanco";
+
+                case Color.Azul:
+                    return "Este es Azul";
+
+                case Color.Negro:
+                    return "Este es Negro";
+
+                default:
+                    return "Color no encontrado";
+            }
+        }
+
+
+
+
+        //  Ejercicio Sentencia if y else 
+
+        public static string EsMayorA(int a)
+        {
+            if (a > 10)
+            {
+                return "Es mayor a 10";
+            }
+            else
+            {
+                return "Error";
+            }
+        }
+
+
+    }
+}
---
+++ Console_App_TP1-master/TestProject_1/ProgramaEJ1.cs
@@ -0,0 +1,271 @@
+using Console_App_TP1;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Drawing;
+using System.Security.Cryptography;
+using static System.Net.Mime.MediaTypeNames;
+using Color = Console_App_TP1.Color;
+
+namespace TestProject_1
+{
+    [TestClass]
+    public class ProgramaEJ1
+    {
+
+        //  Test Ejercicio 1
+
+        [TestMethod]
+        public void Testsumar()
+        {
+
+            // Dados estos valores:
+            int a = 1;
+            int b = 2;
+
+            int esperado = 3;
+
+            int calculado  = Calcs.Sumar(a, b);
+
+            // comparación
+
+            Assert.AreEqual(esperado, calculado);
+
+        }
+
+
+        //  Test Ejercicio 2
+
+        [TestMethod]
+        public void Testcadena()
+        {
+            // Dado            
+            string a = "diversos";
+            string esperado = "dive";            
+            
+            string resultado = Calcs.cadenacortada(a);
+            
+            // comparación
+                        
+            Assert.AreEqual(esperado, resultado);
+
+        }
+
+
+        //  Test Ejercicio 3
+
+        [TestMethod]
+        public void saberhora()
+        {
+            // Dado            
+            
+            string esperado = DateTime.Now.ToString("dd-MM-yy hh:mm");
+                        
+            string resultado = Calcs.quehoraes();
+
+            // comparación
+
+            Assert.AreEqual(esperado, resultado);
+        }
+
+
+
+        //  Test Ejercicio 4 y 5  (no son necesarios)
+
+
+
+        //  Test Ejercicio Enumeración 1 y 2
+
+        [TestMethod]
+        public void Enumerar()
+        {
+            // Dado            
+            string esperado = "Hoy es día Lunes";
+                        
+            string resultado = Calcs.DiasSemana(2);
+
+            // comparación            
+            Assert.AreEqual(esperado, resultado);
+        }
+
+
+
+        //  Test Ejercicio Conversiones 1 parte A
+
+        [TestMethod]
+        public void Conver1()
+        {
+            // Dado
+
+            int verdadero = 1;
+
+            bool esperado = true;
+            
+            bool resultado = Calcs.Conver_V_F(verdadero);
+
+            // comparación
+             
+            Assert.AreEqual(esperado, resultado);
+        }
+
+
+
+        //  Test Ejercicio Conversiones 1  parte B
+
+        [TestMethod]
+        public void Conver2()
+        {
+            // Dado
+            int falso = 0;
+
+            bool esperado = false;
+            
+            bool resultado = Calcs.Conver_V_F(falso);
+
+            // comparación
+
+            Assert.AreEqual(esperado, resultado);
+        }
+
+
+
+        //  Test Ejercicio Conversiones 2
+
+        [TestMethod]
+        public void Conver3()
+        {
+            // Dado
+            string falso = "false";
+            bool esperado = false;
+           
+
+            bool resultado = Calcs.BoolParseado(falso);
+
+            // comparación
+            
+            Assert.AreEqual(esperado, resultado);
+        }
+
+
+
+        //  Test Ejercicio Conversiones 4
+
+
+        [TestMethod]
+        public void Conver4()
+        {
+            // Dado
+            string verdadero = "true";
+
+            bool esperado = true;
+
+            
+            bool resultado = Calcs.BoolParseado(verdadero);
+
+            // comparación
+
+            Assert.AreEqual(esperado, resultado);
+        }
+
+
+
+        // Test Ejercicio Casteo 1
+
+        [TestMethod]
+        public void Casteo1()
+        {
+            // Dado
+            var number = 344.4;
+
+            int esperado = 344;
+
+            int resultado = Calcs.ConvertirDoubleAInt(number);
+
+            // comparación
+
+            Assert.AreEqual(esperado, resultado);
+
+        }
+
+
+        // Test Ejercicio Casteo 2
+
+        [TestMethod]
+        public void Casteo2()
+        {
+            // Dado
+            var number = 10;
+
+            decimal esperado = 10;
+            
+            decimal resultado = Calcs.ConvertirIntADecimal(number);
+
+            // comparación
+            // 
+            Assert.AreEqual(esperado, resultado);
+        }
+
+
+
+        // Test Ejercicio Casteo 3
+
+        [TestMethod]
+        public void Casteo3()
+        {
+            // Dado
+            int numbero = 443444;
+
+            short esperado = -15308;
+            
+            short resultado = Calcs.ConvertirIntAShort(numbero);
+
+            // comparación
+            
+            Assert.AreEqual(esperado, resultado);
+        }
+
+
+
+        // Test Ejercicio Switch 
+
+        [TestMethod]
+        public void Switch1()
+        {
+            // Dado
+
+            Color miColor = Color.Azul;
+
+            string esperado = "Este es Azul";
+            
+            string resultado = Calcs.QueColorEs(miColor);
+
+            // comparación            
+
+            Assert.AreEqual(esperado, resultado);
+
+        }
+
+
+
+
+        // Test Ejercicio Sentencia if y else 
+
+
+        [TestMethod]
+        public void ifyelse()
+        {
+            // Dado
+
+            int numero = 20;
+
+            string esperado = "Es mayor a 10";
+            
+            string resultado = Calcs.EsMayorA(numero);
+
+            // comparación
+            
+            Assert.AreEqual(esperado, resultado);
+
+        }
+
+
+    }
+}
